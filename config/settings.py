6fce3f0f54543cb5960da6d7f839b3b6a5ac3ed8
Manikandan <manikandan.dev.it@gmail.com>
2026-03-28 08:45:05 +0530

    home4

--- config/settings.py
+++ config/settings.py
@@ -81,4 +81,6 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://karaoke-front-end.vercel.app",
+]
